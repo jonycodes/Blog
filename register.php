788f4abdd8c2effa5aa67f9d0b865d77e67f986f
jonycodes <mr.joannier@yahoo.com>
2015-09-03 18:16:16 -0400

    Updated

--- register.php
+++ register.php
@@ -13,8 +13,8 @@ and open the template in the editor.
         <h2>Registration Page</h2>
         <a href="index.php">Go Back</a>
         <form action="register.php" method="POST">
-            Enter username: <input type='text' name='username'  required/>
-            Enter password: <input type='password' name='username' required/>
+            Enter username: <input type='text' name='username' autocomplete="off" required/>
+            Enter password: <input type='password' formaction='register.php' name='password' autocomplete="off" required/>
             First Name: <input type='text' name='fName' value="Required" required/>
             Last Name: <input type='text' name='lName' value="Required" required/>
             <input type='submit' value='register'>
@@ -23,16 +23,35 @@ and open the template in the editor.
             
          <?php
         if ($_SERVER["REQUEST_METHOD"]  == 'POST'){
-                $username = mysql_real_escape_string($_POST['username']);
-                $password = mysql_real_escape_string($_POST['password']);
-                /*echo 'Username enter is : '. $username . "<br/>";
-                echo 'Password entered is: ' . $password ;
-                  */
+
+                $username = mysql_real_escape_string($_POST["username"]);
+                $password = mysql_real_escape_string($_POST["password"]);
+             
                   $bool = true;
-                  mysql_connect('localhost', 'root', ""); or die (mysql_error());
-                  mysql_select_db('first_db') pr die('Cannot Connect to the server');
-                $query =mysql_query("Select * from suers"); 
-        }
+                  $conn = mysql_connect('localhost', 'root', "");
+                       if (!$conn){
+                        die ("could not connect".mysql_error());
+                       } 
+                  mysql_select_db('first_db');
+                  $querry = mysql_query('SELECT * FROM users');
+                      if(!$querry){
+                        die ('could not access data base'.mysql_error());                     
+                  while($row = mysql_fetch_array($querry)){
+                      $table_users = $row['username'];
+                      if($username == $table_users){
+                        $bool = false;
+                         echo "username exists";
+                         //print '<script>windo.location.assign("register.php");</script>';
+                      }
+                    }
+                      if($bool){
+                        mysql_query("INSERT INTO users (username, password) VALUES ('$username', '$password')");
+                        echo "Successfully enrolled";
+                        //print "<script>windows.location.assign('register.php');</script>";
+                      }
+                }
+
+        
         ?>
     </body>
 </html>
