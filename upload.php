5207d5071b6e8b6ddde1627371d4f3626247a01e
Joannier Pinales <mr.joannier@yahoo.com>
2015-10-19 09:50:01 -0400

    debugging

--- upload.php
+++ upload.php
@@ -1,77 +1,77 @@
 
-<form action='upload.php' method='POST' enctype='multipart/form-data'>   
-    <input type='file' name='file'>
-    <input type="submit" value="Add Image" name='file_add'>
-</form>
 
 <?php
- class upload{
-private $name = "";
-private $size = "";
-private $type = "";
-private $location = "";
-private $time = "";  
-private $date = ""; 
+ 
+ $name = "";
+ $size = "";
+ $type = "";
+ $location = "";
+ $time = "";  
+ $date = ""; 
 
-    public function __construct(){
+   
     if (isset($_POST['file'])) {
-        $this->time = date("g:i:s", time());
-        $this->date = date('D M Y');
-        $this->name = $_FILES['file']['name'];
-        $this->size = $_FILES['file']['size'];
-        $this->type = $_FILES['file']['type'];
+        global $time, $date, $name, $size, $type;
+        $time = date("g:i:s", time());
+        $date = date('D M Y');
+        $name = $_FILES['file']['name'];
+        $size = $_FILES['file']['size'];
+        $type = $_FILES['file']['type'];
         if (!empty($name)) {
             return true;
         } else {
-            echo "<script>alert('Please choose  file first');</script>";
-            echo "<script>window.location.assign('home.php');</script";
+           return false;
         }
-    }
-}
+   }
 
 
-private function checkfileType() {
-    $extension = strtolower(substr($this->name, strpos($this->name, '.') + 1));
-    if (($this->type == "image/jpeg" || $this->type == "image/jpg") && ($extension == "jpg" || $extension = "jpeg")) {
+
+function checkfileType() {
+    global $name, $type; 
+    $extension = strtolower(substr($name, strpos($name, '.') + 1));
+    if (($type == "image/jpeg" || $type == "image/jpg") && ($extension == "jpg" || $extension = "jpeg")) {
         return true;
     }
     return false;
 }
 
-private function checkfileSize() {
-    if ($this->size > 4194304) {
-        return false;
+ function checkfileSize() {
+     global $size;
+    if ($size > 4194304) {
+        return true;
     }
-    return true;
+    return false;
 }
 
-public function uploadFile() {       
-        if (!$this->checkfileSize()) {
-                echo "<script>alert('File is over 4MB');</script>";
-                echo "<script>window.location.assign('home.php');</script>";
+ function uploadFile() {    
+     global $location, $name;
+        if ($checkfileSize()) {
                 return false;
             }else
             
-        if ($this->checkfileType()) {
-            $this->location = "uploads/" . $_FILES['file']['name'];
-            $this->name = $_FILES['file']['tmp_name'];
-            move_uploaded_file($this->name, $this->location);
-            addtoServer();
-            echo "<script>alert('File Upload Sucessful');</script>";
-            echo "<script>window.location.assign('home.php');</script>";
-            return true;
-        }else
-        echo 
+            if ($checkfileType()) {
+            $location = "uploads/" . $_FILES['file']['name'];
+            $name = $_FILES['file']['tmp_name'];
+                if(is_uploaded_file($name)){
+                move_uploaded_file($name, $location);
+                addtoServer(); 
+                return true;
+                }
+             }  else
+            return false;
     }
 
 
-private function addtoServer() {
+ function addtoServer() {
     session_start();
     require('connect.php');
     $user = $_SESSION['user'];
-    mysql_query("INSERT INTO list(time_posted, date_posted, file_location, user) VALUES ('$this->time', '$this->date', '$this->location', '$user')") or
+    mysql_query("INSERT INTO list(time_posted, date_posted, file_location, user) VALUES ('$time', '$date', '$location', '$user')") or
             die("Could not insert image into data base".mysql_error());
 }
 
-}
+uploadFile();
+
+header("location: home.php");
+
 ?>
